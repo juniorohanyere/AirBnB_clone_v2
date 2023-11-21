6e4f8881a3199d2a3dc79cad05cfdb0665e89f0b
bennet489 <bennetasare@outlook.com>
2023-11-21 21:08:57 +0300

    debug user in models

--- models/user.py
+++ models/user.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""User Class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
+
 
 class User(BaseModel, Base):
-    """User class
+    """Class for User
     Attributes:
         email: email address
-        password: login password
+        password: password for you login
         first_name: first name
         last_name: last name
-    """"
+    """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
