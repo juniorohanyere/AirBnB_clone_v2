c59e739f67e5427c4f1e9776403aa4a384159cd9
bennet489 <bennetasare@outlook.com>
2023-11-21 21:25:12 +0300

    bug fix on user model

--- models/user.py
+++ models/user.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-"""User Class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+
+"""
+A module that defines the ORM class for User table
+"""
+from os import getenv
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 
 
 class User(BaseModel, Base):
-    """Class for User
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
     """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+    Defines attributes for User table
+    """
+    __tablename__ = 'users'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship(
+            'Place', backref='user', cascade='all, delete')
+        reviews = relationship(
+            'Review', backref='user', cascade='all, delete')
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
