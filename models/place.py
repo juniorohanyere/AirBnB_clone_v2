51972dc72ec51c4ad1322686ee39bc79c13e2424
Twin J <junohanyere@gmail.com>
2023-11-22 14:22:18 +0100

    Revert "modified"
    This reverts commit a77f1134c31a260d7ac8281676be762db6ad4bb1.

--- models/place.py
+++ models/place.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """This is the place class"""
 from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
-
 import models
-from models.base_model import BaseModel, Base
 
 
 place_amenity = Table("place_amenity", Base.metadata,
@@ -48,36 +47,37 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     amenity_ids = []
 
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="place")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                               backref="place")
 
-    amenities = relationship("Amenity", secondary=place_amenity,
-                             viewonly=False,
-                             back_populates="place_amenities")
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
+    else:
+        @property
+        def reviews(self):
+            """ Returns list of reviews.id """
+            var = models.storage.all()
+            lista = []
+            result = []
+            for key in var:
+                review = key.replace('.', ' ')
+                review = shlex.split(review)
+                if (review[0] == 'Review'):
+                    lista.append(var[key])
+            for elem in lista:
+                if (elem.place_id == self.id):
+                    result.append(elem)
+            return (result)
 
-    @property
-    def reviews(self):
-        """ Returns list of reviews.id """
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            review = key.replace('.', ' ')
-            review = shlex.split(review)
-            if (review[0] == 'Review'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.place_id == self.id):
-                result.append(elem)
-        return (result)
+        @property
+        def amenities(self):
+            """ Returns list of amenity ids """
+            return self.amenity_ids
 
-    @property
-    def amenities(self):
-        """ Returns list of amenity ids """
-        return self.amenity_ids
-
-    @amenities.setter
-    def amenities(self, obj=None):
-        """ Appends amenity ids to the attribute """
-        if type(obj) is Amenity and obj.id not in self.amenity_ids:
-            self.amenity_ids.append(obj.id)
+        @amenities.setter
+        def amenities(self, obj=None):
+            """ Appends amenity ids to the attribute """
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
