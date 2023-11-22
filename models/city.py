685c859a089c3e52ed15473d845991ae0fa6fda5
Twin J <junohanyere@gmail.com>
2023-11-22 12:40:31 +0100

    Revert "debug"
    This reverts commit 570ca01f6af5af341457d0e7837f31f9d9e7fff2.

--- models/city.py
+++ models/city.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import Basemodel, Base
-from models import storage_type
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+
+from models.base_model import Base
+from models.base_model import BaseModel
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+
     __tablename__ = 'cities'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities',
-                              cascade='all, delete, delete-orphan')
-    else:
-        name = ''
-        state_id = ''
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
